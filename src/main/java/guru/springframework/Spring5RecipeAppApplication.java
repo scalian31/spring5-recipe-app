36f75db8390c71863a442ed08370ca16e348113d
tb31 <32811762+tb31@users.noreply.github.com>
2017-10-18 08:03:53 +0200

    Update Spring5RecipeAppApplication.java
    test commentaire

--- src/main/java/guru/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5RecipeAppApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
+	
+	//OK it works
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
